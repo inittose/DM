d2f5060d262dc809496f5361e14d9394dc7dc416
inittose <pol.konnik@gmail.com>
2023-12-13 00:03:31 +0700

    Начал 4 лабу

--- Лабораторная работа 4/lab4.py
+++ Лабораторная работа 4/lab4.py
@@ -50,16 +50,6 @@ def CopyMatrix(matrix, n):
             newMatrix[i][j] = matrix[i][j]
     return newMatrix
 
-def SortCapacities(array):
-    result = copy.deepcopy(array)
-    for i in range(len(result)):
-        for j in range(i, len(result)):
-            if len(result[i]) < len(result[j]):
-                temp = result[i]
-                result[i] = result[j]
-                result[j] = temp
-    return result
-
 class Graph:
     # Конструктор класса
     def __init__(self, size, bFullGraph = False, bMultiedge = False, bLoop = False):
@@ -87,39 +77,19 @@ class Graph:
                 else:
                     print(end = " ")
             print()
-    
-    # Вывод матрицы смежности
-    def showMetricMatrix(self):
-        print("Матрица метрики: ")
-        for i in range(self._nodes * 2 + 1):
-            for j in range(self._nodes * 2 + 1):
-                if j % 2 == 1:
-                    print(end = "  ") # |
-                elif i % 2 == 1:
-                    print(end = " ") # -
-                elif (i == 0 and j != 0) or (j == 0 and i != 0):
-                    print(end = f"{chr(AINDEX + (i + j) // 2 - 1)}")
-                elif i // 2 > 0 and j // 2 > 0:
-                    print(end = f"{self._mmatrix[i // 2 - 1][j // 2 - 1]}")
-                else:
-                    print(end = " ")
-            print()
 
     # Вывод графа (при помощи networkx)
     def showGraph(self):
         nodeMap = dict()
         edgeMap = dict()
         loopMap = dict()
-        colorMap = list()
-        for i in range(self._nodes):
-            colorMap.append("blue")
 
         for i in range(0, self._nodes):
             nodeMap.update({i: chr(AINDEX + i)})
 
         count = 1
         for i in range(self._nodes):
-            for j in range(i, self._nodes):
+            for j in range(self._nodes):
                 if self._amatrix[i][j] != 0:
                     edgeName = ""
                     if self._amatrix[i][j] > 1:
@@ -137,14 +107,10 @@ class Graph:
                     else:
                         edgeMap.update({(chr(AINDEX + i), chr(AINDEX + j)): edgeName})
         
-        for i in range(len(self._colorMap)):
-            for j in range(len(self._colorMap[i])):
-                colorMap[ord(self._colorMap[i][j]) - AINDEX] = COLORS[i]
-        print(colorMap)
         G = nx.Graph(np.array(self._amatrix))
         nx.relabel_nodes(G, nodeMap, False)
         pos = nx.circular_layout(G)
-        nx.draw(G, pos, with_labels = True, node_color = colorMap)
+        nx.draw(G, pos, with_labels = True, arrows = True, arrowstyle = '-|>')
         nx.draw_networkx_edge_labels(G, pos, edge_labels = edgeMap)
         nx.draw_networkx_edge_labels(G, pos, edge_labels = loopMap)
         plt.show()
@@ -155,16 +121,16 @@ class Graph:
         minEdges = 1 if self._bFullGraph else 0
         maxEdges = 3 if self._bMultiedge else 1
 
-        for i in range(0, self._nodes):
-            for j in range(i + deltaIndex, self._nodes):
+        for i in range(self._nodes):
+            for j in range(self._nodes):
                 value = random.randint(1, maxEdges) if random.randint(minEdges, 1) == 1 else 0
-                self._amatrix[i][j] = self._amatrix[j][i] = value
+                self._amatrix[i][j] = value if (i != j or deltaIndex == 0) else 0
     
     # Подсчет ребер графа
     def updateEdges(self):
         edges = 0
         for i in range(self._nodes):
-            for j in range(i, self._nodes):
+            for j in range(self._nodes):
                 edges += self._amatrix[i][j]
         self._edges = edges
     
@@ -180,14 +146,12 @@ class Graph:
         
         edgeIndex = 0
         for i in range(self._nodes):
-            for j in range(i, self._nodes):
+            for j in range(self._nodes):
                 if self._amatrix[i][j] != 0:
-                    value = 1
-                    if i == j:
-                        value = 2
                     
                     for k in range(self._amatrix[i][j]):
-                            self._imatrix[i][edgeIndex] = self._imatrix[j][edgeIndex] = value
+                            self._imatrix[i][edgeIndex] = -1
+                            self._imatrix[j][edgeIndex] = 1
                             edgeIndex += 1
 
     # Вывод матрицы инцидентности
@@ -206,111 +170,67 @@ class Graph:
                 elif i == 0 and j != 0:
                     print(end = f"{j // 2}")
                 elif i // 2 > 0 and j // 2 > 0:
+                    if self._imatrix[i // 2 - 1][j // 2 - 1] < 0:
+                        print(end = '\b')
                     print(end = f"{self._imatrix[i // 2 - 1][j // 2 - 1]}")
                     if j >= 20:
                         print(end = " ")
                 else:
                     print(end = " ")
             print()
-    
-    # Создаение матрицы метрики
-    def MakeMetricMatrix(self):
-        self._mmatrix = None
-        tempMatrix = MakeMatrix(self._nodes)
-        smatrix = MakeMatrix(self._nodes)
-        k = 1
+
+    def MakeWeightMatrix(self):
+        self._weightMatrix = MakeMatrix(self._nodes)
         for i in range(self._nodes):
             for j in range(self._nodes):
-                smatrix[i][j] = 1 if self._amatrix[i][j] != 0 else 0
-                if i == j:
-                    smatrix[i][j] += 1
-        while not EqualMatrix(self._mmatrix, tempMatrix, self._nodes):
-            self._mmatrix = CopyMatrix(tempMatrix, self._nodes)
-            for i in range(self._nodes):
+                if i != j:
+                    if self._amatrix[i][j] == 0:
+                        self._weightMatrix[i][j] = math.inf
+                    else:
+                        self._weightMatrix[i][j] = random.randint(0, 5)
+
+    # Вывод матрицы весов
+    def showWeightMatrix(self):
+        if self._weightMatrix == None:
+            self.MakeWeightMatrix()
+        print("Матрица весов: ")
+        for i in range(self._nodes * 2 + 1):
+            for j in range(self._nodes * 2 + 1):
+                if j % 2 == 1:
+                    print(end = "   ") # |
+                elif i % 2 == 1:
+                    print(end = " ") # -
+                elif (i == 0 and j != 0) or (j == 0 and i != 0):
+                    print(end = f"{chr(AINDEX + (i + j) // 2 - 1)}")
+                elif i // 2 > 0 and j // 2 > 0:
+                    if self._weightMatrix[i // 2 - 1][j // 2 - 1] == math.inf:
+                        print(end = "\b")
+                    print(end = f"{self._weightMatrix[i // 2 - 1][j // 2 - 1]}")
+                else:
+                    print(end = " ")
+            print()
+    
+    def DijkstraAlgorithm(self, start, end):
+        DijkstraMatrix = MakeMatrix(self._nodes)
+        mask = ''
+        for k in range(self._nodes):
+            mask += chr(AINDEX + k)
+
+        currentNode = ord(start) - AINDEX
+        for i in range(self._nodes):
+            if i == 0:
                 for j in range(self._nodes):
-                    if (not i==j and smatrix[i][j] != 0 and tempMatrix[i][j] == 0):
-                        tempMatrix[i][j] += k
-            smatrix = PowerMatrix(smatrix, self._nodes)
-            k+=1 
+                    DijkstraMatrix[i][j] = self._weightMatrix[currentNode][j]
+            else:
+                pass
 
-    # Нахождение и вывод метрик (центральные\переферийные точки, радиус, диаметр)
-    def FindMetrics(self):
-        maxRow = list()
-        for i in range(self._nodes):
-            maxRow.append(max(self._mmatrix[i]))
-        self.radius = min(maxRow) if min(maxRow) != 0 else math.inf
-        self.diametr = max(maxRow) if self.radius != math.inf else math.inf
-        print (f"radius = {self.radius}, diametr = {self.diametr}")
-        self.peripheral = list()
-        self.central = list()
-        for i in range(self._nodes):
-            if max(self._mmatrix[i]) == self.radius or self.radius == math.inf:
-                self.central.append(chr(AINDEX + i))
-            if max(self._mmatrix[i]) == self.diametr or self.diametr == math.inf:
-                self.peripheral.append(chr(AINDEX + i))
-        print (f"center = {self.central}; peripheral = {self.peripheral}")
-    
-    # Поиск максимально пустых графов
-    def FindEmptyGraphs(self):
-        self._emptyGraphs = list()
-        polynomial = ""
-        mask = ""
-        for i in range(self._nodes):
-            mask += chr(AINDEX + i)
-        for i in range(self._edges):
-            split = True
-            polynomial += "("
-            for j in range(self._nodes):
-                if self._imatrix[j][i] == 1:
-                    polynomial += chr(AINDEX + j)
-                    if split:
-                        polynomial+= " + "
-                        split = False
-            polynomial += ")"
-        print(polynomial)
-        transformations = standard_transformations + (implicit_multiplication_application,)
-        polynomial = str(sp.expand(parse_expr(polynomial, transformations=transformations)))
-        print(polynomial)
-        temp = ""
-        for i in range(len(polynomial)):
-            if polynomial[i] != "*" and not polynomial[i].isdigit():
-                temp += polynomial[i]
-        polynomial = temp
-        multipliers = polynomial.split(" + ")
-        print(polynomial)
-        for i in range(len(multipliers)):
-            if len(multipliers[i]) + 1 >= self._nodes:
-                continue 
-            temp = ""
-            for j in range(self._nodes):
-                if mask[j] not in multipliers[i]:
-                    temp += mask[j]
-            if temp not in self._emptyGraphs:
-                self._emptyGraphs.append(temp)
+            temp = ''
+            for k in range(len(mask) - 1):
+                temp += mask[k] if mask[k] != chr(currentNode + AINDEX) else ''
+            mask = temp
 
-        print(f"S = {self._emptyGraphs} - максимально пустые подграфы")
-    
-    def GraphColoring(self):
-        if self._emptyGraphs == None:
-            self.FindEmptyGraphs()
-        colorMap = list()
-        counter = 0
-        colorNodes = SortCapacities(self._emptyGraphs)
-        while colorNodes and len(colorNodes[0]) != 0:
-            print(colorNodes)
-            oneColor = colorNodes.pop(0)
-            colorMap.append(oneColor)
-            counter += 1
-            for i in range(len(colorNodes)):
-                temp = ""
-                for j in range(len(colorNodes[i])):
-                    if colorNodes[i][j] not in oneColor:
-                        temp += colorNodes[i][j]
-                colorNodes[i] = temp
-            colorNodes = SortCapacities(colorNodes)
-        print(colorMap)
-        print(f"G = {len(colorMap)} - хроматическое число графа")
-        self._colorMap = colorMap
+            
+            
 
 
         
@@ -346,11 +266,8 @@ def main():
         graph.showAdjacencyMatrix()
         print()
         graph.showIncidenceMatrix()
-        graph.FindEmptyGraphs()
-        graph.GraphColoring()
-        graph.MakeMetricMatrix()
-        graph.showMetricMatrix()
-        graph.FindMetrics()
+        graph.MakeWeightMatrix()
+        graph.showWeightMatrix()
         graph.showGraph()
         os.system("cls")
         mode = input(menu)
